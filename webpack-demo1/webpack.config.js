1e864d587fecc6316132cfdbfe59bd86ed53ecb4
wrq <843616590@qq.com>
2022-07-04 18:05:45 +0800

    test1

--- webpack-demo1/webpack.config.js
+++ webpack-demo1/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title:'Caching'
+    })
+  ]
 };
