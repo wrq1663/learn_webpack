a288f0055e387103cccadd4a3b014cae8781225c
wrq <843616590@qq.com>
2022-07-08 18:31:35 +0800

    周五下班了 有事下周说

--- webpack-sgg1/src/index.js
+++ webpack-sgg1/src/index.js
@@ -1,5 +1,6 @@
 import './css/index.css'
 import './css/test.less'
 import './css/test2.scss'
-
+import './assets/icon/iconfont.css'
 console.log(123123)
+// var a = 123
--- webpack-sgg1/webpack.config.js
+++ webpack-sgg1/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'js/main.js',
+    clean: true
   },
   module: {
     rules: [
@@ -16,24 +18,57 @@ module.exports = {
         ]
       },
       {
-        test:/\.less$/,
+        test: /\.less$/,
         //loader:''  =>这个是一个loader时可以使用
-        use:[
+        use: [
           'style-loader',
           'css-loader',
           'less-loader'
         ]
       },
       {
-        test:/\.s[ac]ss$/,
-        use:[
+        test: /\.s[ac]ss$/,
+        use: [
           'style-loader',
           'css-loader',
           'sass-loader'
         ]
       },
+      {
+        test: /.(png|jpg?g|gif|webp)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024,
+          },
+        },
+        generator: {
+          filename: "static1/[hash:6][ext][query]"
+        }
+      },
+      {
+        test: /.(ttf|woff2?)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: "static1/media/[hash:10][ext][query]"
+        }
+      },
+      {
+        test:/\.js$/,
+        use:{
+          loader:"babel-loader",
+          options:{
+            presets:['@babel/perset-env']
+          }
+        }
+      }
     ]
   },
-  plugins: [],
+  plugins: [
+    new ESLintPlugin({
+      //检测哪些文件
+      context:path.resolve(__dirname, "src")
+    })
+  ],
   mode: "development"
 }
